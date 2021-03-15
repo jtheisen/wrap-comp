848ff840f364425048e404246fe5da971ec16a65
jtheisen <pass-git@zokfotpik.de>
2021-03-15 13:34:37 +0100

    cleanup and readme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { wrapComp } from './App';
+import { Button, Collapse, Divider, Text } from '@blueprintjs/core';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function testSimple(element: React.ReactElement, expected: string) {
+  test('', () => expect(render(element).container.innerHTML).toEqual(expected))
+}
+
+export const Row = wrapComp("div", "flex");
+export const RowReverse = wrapComp(Row, "flex-row-reverse space-x-reverse");
+export const Col = wrapComp(Row, "flex-col");
+export const ColReverse = wrapComp(Row, "flex-col-reverse space-y-reverse");
+
+testSimple(<Row />, '<div class="flex"></div>')
+testSimple(<RowReverse />, '<div class="flex flex-row-reverse space-x-reverse"></div>')
+testSimple(<Col />, '<div class="flex flex-col"></div>')
+testSimple(<ColReverse />, '<div class="flex flex-col-reverse space-y-reverse"></div>')
+
+export const TextWithExtras = wrapComp(Text, p => p.ellipsize ? "x-ellipsize" : "x-raw");
+
+testSimple(
+  <TextWithExtras ellipsize={false}>content</TextWithExtras>,
+  "<div class=\"x-raw\">content</div>"
+)
+testSimple(
+  <TextWithExtras ellipsize={true}>content</TextWithExtras>,
+  "<div class=\"bp3-text-overflow-ellipsis x-ellipsize\">content</div>"
+)
+
+// Giving default props doesn't yet work: The object argument's prop values are sometimes too broad (string) or too narrow ("trash").
+//export const Input = wrapComp(Icon, { icon: "trash" })
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classnames from "classnames";
+import * as React from "react";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type ClassName = undefined | string;
+
+type ClassNameDefinition<P> = ClassName | ((props: P) => ClassName);
+
+export function wrapComp<E extends keyof JSX.IntrinsicElements>(
+  element: E,
+  className?: ClassNameDefinition<JSX.IntrinsicElements[E]>
+): React.FunctionComponent<JSX.IntrinsicElements[E]>;
+export function wrapComp<P extends { className?: string }>(
+  component: React.ComponentType<P>,
+  className?: ClassNameDefinition<P>
+): React.ComponentType<P>;
+
+// The implementation attempts to do more than the function
+// prototypes express: The second parameters can also be a props
+// object. I didn't manage to get the typings correct for that
+// usage though.
+export function wrapComp(
+  elementOrComponent: any,
+  classNameOrPropsDefinition: any
+): any {
+  const Component = elementOrComponent;
+  function Comp(props: any) {
+    const classNameOrProps =
+      typeof classNameOrPropsDefinition === "function"
+        ? classNameOrPropsDefinition(props)
+        : classNameOrPropsDefinition;
+
+    const outerProps =
+      typeof classNameOrProps === "object"
+        ? {
+            ...classNameOrProps,
+            className: classnames(props.classNameconfigObject["className"]),
+          }
+        : typeof classNameOrProps === "string"
+        ? { className: classnames(classNameOrProps, props.className) }
+        : {};
+    return <Component {...props} {...outerProps} />;
+  }
+  return Comp;
 }
 
-export default App;
+export const Row = wrapComp("div", "flex flex-row");
+export const RowReverse = wrapComp(Row, "flex-row-reverse space-x-reverse");
+export const Col = wrapComp("div", "flex flex-col");
+export const ColReverse = wrapComp(Col, "flex-col-reverse space-y-reverse");
+
+export default function App() {
+  const [flag, setFlag] = useState(false);
+
+  return (
+    <Row className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </Row>
+  );
+}
